2d3df95cd182e312c041e72f76240f7709cebc61
tania.limachi@ucb.edu.bo <tania@WINDOWS-2T5J4JB.localdomain>
2025-11-19 16:23:16 -0400

    modificacion de archivos jason y archivo javascript

--- funciones/script.js
+++ funciones/script.js
@@ -1,5 +1,35 @@
 //Funcionalidades
- 
+
+
+// Inicio 
+const URL_INICIO = '../datos/informacion.json'; 
+
+async function cargarInicio() {
+  try {
+    const respuesta = await fetch(URL_INICIO);
+    const data = await respuesta.json();
+
+    const inicio = data['primera-parte']; 
+    const contenedor = document.getElementById('hero');
+
+    const articulo = document.createElement('article');
+
+    articulo.innerHTML =
+      "<img src='" + inicio['img-hero'] + "' alt='Ilustración'>" +
+      "<h1>" + inicio.titular + "</h1>" +
+      "<h4>" + inicio.categoria + "</h4>" +
+      "<p>" + inicio['frase-descriptiva'] + "</p>";
+
+    contenedor.appendChild(articulo);
+
+  } catch (error) {
+    console.error("Error al cargar el proyecto:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", cargarInicio);
+
+
 
 // Sobre proyecto
 const URL_PROY = '../datos/sobre-proyecto.json'; 
